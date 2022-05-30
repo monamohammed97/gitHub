47382f13d6883e801fa9bfb9549811ddef27dc25
Taher-Abu-Tayyem <100804250+Taher-Abu-Tayyem@users.noreply.github.com>
2022-05-30 13:44:29 +0300

    home div

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React from "react"
 function App() {
   return (
-    <div className="Nav-div">
-      <p className="Nav-Title">audiophile</p>
-      <h1></h1>
+    <div className="Nav-container">
       
+    <div className="Nav-div">
+      <p className="Nav-Title">Audiophile</p>
+      <ul className="Nav-uL">
+        <li>HOME</li>
+        <li>HEADPHONES</li>
+        <li>SPEAKERS</li>
+        <li>EARPHONES</li>
+      </ul>
+      </div>
+      <hr></hr>
+
+      <div className="Home">
+        <div className='Home-div'>
+        <h4>NEW PRODUCT</h4>
+        <h1>X99 MARK II HEADPHONES</h1>
+        <p>X99 MARK II HEADPHONESX99 MARK II HEADPHONESX99 MARK II HEADPHONESX99 MARK II HEADPHONES</p>
+        <button >SEE PRODUCT</button>
+        </div>
+      </div>
+   
+
+   
     </div>
+
+
   );
 }
 
